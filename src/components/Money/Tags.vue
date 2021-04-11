bca3c9f10a8bbac3459d0df8c18be9c8a00e1265
Coder-XO <coderless@gmail.com>
2021-04-11 14:30:05 +0800

    保存了数据到localStorage里面

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -27,22 +27,28 @@ export default class Tags extends Vue {
     } else {
       this.selectedTags.push(tag);
     }
-    this.$emit('update:value',this.selectedTags)
+    this.$emit('update:value', this.selectedTags);
   }
 
   create() {
     const name = window.prompt('请输入标签名字(最多四个字)');
-    if (name === null) {
-      window.alert('标签名不能为空');
-    } else if (name.length <= 4) {
-      if(this.dataSource){
-        console.log(9);
-        this.$emit('update:value', [...this.dataSource, name]);
-      }else {
-        console.log(999);
+    if (name !== null) {
+      if (name.length <= 4) {
+        if (this.dataSource) {
+          this.$emit('update:dataSource', [...this.dataSource, name]);
+        } else {
+          return;
+        }
+      }else if(name.length > 4){
+        alert('标签名过长')
+        return;
       }
-    }else {
-      alert('标签名不符合')
+      if (name === '') {
+        alert('标签名不能为空');
+      }
+    } else {
+      if (typeof name === 'object')
+        return;
     }
   }
 }
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout class-prefix="layout"><!-- 向子组件中传递类名来控制CSS -->
     {{ record }}
-    <Number-pad :value.sync="record.amount"/>
+    <Number-pad :value.sync="record.amount" @submit="saveRecord"/>
     <Types :value.sync="record.type"/>
     <Notes @update:value="onUpdateNotes"/>
     <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
@@ -13,7 +13,7 @@ import NumberPad from '@/components/Money/NumberPad.vue';
 import Types from '@/components/Money/Types.vue';
 import Notes from '@/components/Money/Notes.vue';
 import Tags from '@/components/Money/Tags.vue';
-import {Component} from 'vue-property-decorator';
+import {Component,Watch} from 'vue-property-decorator';
 
 type Record = {     // TS声明类型
   tags: string[],
@@ -27,6 +27,7 @@ type Record = {     // TS声明类型
 })
 export default class Money extends Vue {
   tags = ['衣', '食', '住', '行'];
+  recordList: Record[] = [];     //  保存用户账目数据
   record: Record = {
     tags: [],
     notes: '',
@@ -35,6 +36,7 @@ export default class Money extends Vue {
   };
 
   onUpdateTags(value: string[]) {
+    console.log(5566558989);
     this.record.tags = value;
   }
 
@@ -44,6 +46,16 @@ export default class Money extends Vue {
 
   onUpdateAmount(value: string) {
     this.record.amount = parseFloat(value);
+  }
+
+  saveRecord() {
+    const deepClone = JSON.stringify(this.record);
+    this.recordList.push(JSON.parse(deepClone));
+  }
+
+  @Watch('recordList')
+  onRecordListChange(){
+    window.localStorage.setItem('recordList', JSON.stringify(this.recordList));     // 本地保存数据
   }
 
 }
