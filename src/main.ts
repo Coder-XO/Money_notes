22239ca7f0f2f69a375a1aa65f4fe8ed5deaa205
Coder-XO <coderless@gmail.com>
2021-05-01 19:39:36 +0800

    âdeploy faild

--- src/main.ts
+++ src/main.ts
@@ -14,13 +14,13 @@ Vue.component('Layout', Layout);   //   全局引入 Layout 组件
 Vue.component('Icon', Icon);   //  Nav 组件
 
 new Vue({
-    router,
-    store,     // 解构赋值
-    render: h => h(App)
+  router,
+  store,     // 解构赋值
+  render: h => h(App)
 }).$mount('#app');
 
 window.onload = function () {    // 帮用户下拉到底部
-    setTimeout(function () {
-        window.scrollTo(0, 200);
-    }, 0);
+  setTimeout(function () {
+    window.scrollTo(0, 300);
+  }, 0);
 };
